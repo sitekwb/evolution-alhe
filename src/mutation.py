9e08ad2f11608a492d5413439c6ee0a355bf08c7
Wojciech <s.wojciech9@gmail.com>
2021-01-25 11:55:18 +0100

    logger debug for mutation

--- src/mutation.py
+++ src/mutation.py
@@ -9,11 +9,17 @@ def mutation(chromosome):
     p = random.uniform(0, 1)
     # mutate only with MUTATION_PROB
     if p < MUTATION_PROB:
+        logger.debug(f'Mutating! Mutation prob is {MUTATION_PROB}')
         # choose one gene for mutation         
         index = random.randrange(len(chromosome))
+        logger.debug(f"Random index: {index}")
+        chosen_chromosome = chromosome[index] # for debug
+        logger.debug(f"Chosen chromosome: {chosen_chromosome}")
         # mutate chosen gene
         if settings["DISTRIBUTED"]:
             chromosome[index] = random.uniform(0, 1)
         else:
             chromosome[index] = random.randrange(len(demand_array[index]))
+        chosen_chromosome = chromosome[index] # for debug
+        logger.debug(f"Randomly mutated gene: {chosen_chromosome}")
     return chromosome
