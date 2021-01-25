0048f66977231fc1e0f998b8d8c36b90c45ae9f7
Wojciech <s.wojciech9@gmail.com>
2021-01-25 11:51:37 +0100

    debug in objectivefunc

--- src/objectivefunc.py
+++ src/objectivefunc.py
@@ -3,13 +3,16 @@ from settings import settings, get_logger
 logger = get_logger('objectivefunc')
 
 
+# returns true if stop
 def stop_condition(generation, stale_generations, fitness):
     if settings["MAX_GENERATIONS"] < generation:
+        logger.debug("Max generations count achieved. Stopping.")
         return True
     if settings["MAX_STALE_GENERATIONS"] < stale_generations:
+        logger.debug("Max stale generations count achieved. Stopping.")
         return True
     if settings["TARGET_FITNESS"] > fitness:    # minimizing fitness
+        logger.debug("Target fitness achieved. Stopping.")
         return True
     else:
         return False
-    # returns true if stop
